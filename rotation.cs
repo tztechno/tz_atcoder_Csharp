7ba1a771da7ead47a77b90b5026532e60eee7e17
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-22 12:59:04 +0900

    Create rotation.cs

---
+++ rotation.cs
@@ -0,0 +1,40 @@
+#ABC181_A
+#rotation
+#input= 5
+
+#######################################
+
+
+using System;
+class Program
+{
+    static void Main()
+    {
+    string input = Console.ReadLine()?.Trim();
+    int N = int.Parse(input);
+        
+    if (N%2==0) {
+        Console.WriteLine("White");
+        }
+    else {
+        Console.WriteLine("Black");
+        }
+    }
+}
+
+
+#######################################
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("White");
+}
+else
+{
+    Console.WriteLine("Black");
+}
+
+
+#######################################
