f7649db85a5a3bc47caf7afae8d9123cdb7126f3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-10 14:46:01 +0900

    Create abc166_a.cs

---
+++ abc166_a.cs
@@ -0,0 +1,38 @@
+//abc166_a.cs
+################################
+################################
+################################
+################################
+################################
+using System;
+namespace ABC166A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            string ans;
+            if (s == "ABC")
+                ans = "ARC";
+            else
+                ans = "ABC";
+            Console.WriteLine(ans);
+        }
+    }
+}
+################################
+Console.WriteLine((Console.ReadLine() == "ABC") ? "ARC" : "ABC");
+################################
+class Problem
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        var ans = "";
+        if (s=="ABC") ans = "ARC";
+        else ans = "ABC";
+        Console.WriteLine(ans);
+    }
+}
+################################
